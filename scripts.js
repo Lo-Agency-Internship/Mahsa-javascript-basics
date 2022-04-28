8b263f95df7c651c57799d8ef65672cdd425ea07
mahsa abadian <abadian.ma@gmail.com>
2022-04-28 15:08:40 +0430

    variable assignment

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
-let officialName = prompt("What is officila name of javascripts?");
+let officialName = prompt("What is official name of javascripts?");
 alert(officialName);
+
+// variables
+
+let fullName = prompt("write down your name? ");
+let age = prompt("write down your age? ");
+let sex = prompt("write down your sex? ");
+alert(fullName + " "+ age + " " + sex);
+
+
+let workDay = prompt("How many days of the week do you work?");
+let offDay = prompt("How many days of the week are you off? ");
+alert("workDay : "+ workDay + " offDay : " + offDay);
+
+
+let saveValue = workDay;
+workDay = offDay;
+offDay = saveValue;
+alert("workDay: " + workDay + " offDay: " + offDay);
